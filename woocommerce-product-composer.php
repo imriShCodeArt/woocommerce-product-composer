02a469569d3a1267981ae0f056e69bcea6d39501
imriSh <59433290+imriShCodeArt@users.noreply.github.com>
2025-07-02 11:49:05 +0300

    docs: update brand's name

--- woocommerce-product-composer.php
+++ woocommerce-product-composer.php
@@ -3,7 +3,7 @@
  * Plugin Name: WooCommerce Product Composer
  * Description: Allows customers to compose product packages by selecting related accessories directly on product pages.
  * Version: 1.0.0
- * Author: Your Name
+ * Author: M.L Web Solutions
  * Text Domain: woocommerce-product-composer
  * Domain Path: /languages
  */
